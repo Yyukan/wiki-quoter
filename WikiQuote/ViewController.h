bf265b5c7f30551734017a84be903131921d05fc
Oleksandr Shtykhno <oleksandrshtykhno@Oleksandr-Shtykhnos-MacBook-Pro.local>
2012-05-24 19:52:26 +0200

    Initial Commit

---
+++ WikiQuote/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WikiQuote
+//
+//  Created by Oleksandr Shtykhno on 24/05/2012.
+//  Copyright (c) 2012 shtykhno.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
