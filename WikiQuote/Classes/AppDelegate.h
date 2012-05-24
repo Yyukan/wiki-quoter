7fad907e9ef841e313f856551b915630df61da3f
Oleksandr Shtykhno <oleksandrshtykhno@Oleksandr-Shtykhnos-MacBook-Pro.local>
2012-05-24 20:26:53 +0200

    Moved source files to Classes

---
+++ WikiQuote/Classes/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WikiQuote
+//
+//  Created by Oleksandr Shtykhno on 24/05/2012.
+//  Copyright (c) 2012 shtykhno.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
